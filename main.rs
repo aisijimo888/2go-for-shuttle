1c77f3cfa1f5efc6eda63204f84e2d02979a0717
aisijimo888 <127577828+aisijimo888@users.noreply.github.com>
2025-07-08 10:42:36 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -20,10 +20,11 @@ async fn setup_environment() {
         ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
         ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
         ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
-        ("ARGO_DOMAIN", ""),    // argo固定隧道域名，留空将使用临时隧道
-        ("ARGO_AUTH", ""),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
+        ("ARGO_DOMAIN", "shuttle.graygray.eu.org"),    // argo固定隧道域名，留空将使用临时隧道
+        ("ARGO_AUTH", "eyJhIjoiYWJmZGRiMGY3NzdmYzQzZDhjOGJlZmY4Zjc1MTE5YzEiLCJ0IjoiODMxOWMzNWYtZjdlMS00NGIzLWI1ZDAtMjRhNjQ5MmJiYjQyIiwicyI6IlltRm1ORFUxTnprdE9UUTFOQzAwWlRsaExXRTROemd0WTJRMVlXRTFNakJpTldVNCJ9
+"),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
-        ("CFIP", "time.is"),    // 优选域名或优选ip
+        ("CFIP", "www.visa.com.tw"),    // 优选域名或优选ip
         ("CFPORT", "443"),      // 优选域名或优选ip对应的端口
         ("NAME", "Shuttle"),    // 节点名称
         ("FILE_PATH", "./tmp"), // 运行目录，保持不变
